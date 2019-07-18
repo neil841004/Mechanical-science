7c4fa1396fa54eada7b2be216a79a9544b5f8b67
Halaa <neil841004@gmail.com>
2019-07-18 21:43:27 +0800

    基本流程完成

---
+++ Assets/BGswitch.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGswitch : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void SwitchBackground(int j)
+    {
+        for (int i = 0; i <= 12; i++)
+        {
+            this.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        this.transform.GetChild(j).gameObject.SetActive(true);
+    }
+	public void SwitchBackgroundWidhBalck(int h)
+    {
+        for (int i = 0; i <= 11; i++)
+        {
+            this.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        this.transform.GetChild(h).gameObject.SetActive(true);
+		this.transform.GetChild(12).gameObject.SetActive(true);
+    }
+}
