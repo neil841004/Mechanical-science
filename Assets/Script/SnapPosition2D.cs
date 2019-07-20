f46297c40288fd6a55f135d44c0eba456d51e065
smark <smark12007@gmail.com>
2019-07-21 00:31:07 +0800

    add dragger scripts and sample scene

---
+++ Assets/Script/SnapPosition2D.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public sealed class SnapPosition2D : MonoBehaviour {
+
+	public Collider2D[] snapAreas = new Collider2D[0];
+	public Dragger2D[] correntAnswers = new Dragger2D[0];
+	[NonSerialized] public Dragger2D currentAnswer = null;
+}
