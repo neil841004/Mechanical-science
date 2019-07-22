6df98800e7cfd77f783ae2228695b7b51e9af7c9
Halaa <neil841004@gmail.com>
2019-07-22 23:45:18 +0800

    完成拖曳關卡鎖定、重置功能

---
+++ Assets/Script/DragObj.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragObj : MonoBehaviour {
+	public int objAmount = 0;
+	
+	public void ResetDragObj(){
+		for (int i = 0; i < objAmount; i++)
+		{
+			this.transform.GetChild(i).GetComponent<Dragger2D>().ResetPosition();
+		}
+	}
+}
